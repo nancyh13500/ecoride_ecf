c28d0aaa0bcd92c5fbd7eed0aa64b75640d2cc90
nancyh13500 <nancy.hanoyan@free.fr>
2025-03-23 17:39:29 +0100

    update

--- index.php
+++ index.php
@@ -2,16 +2,27 @@
 ?>
 
 <!--section search -->
-<div class="search">
-    <div class="p-5 text-center bg-body-tertiary">
-        <div class="container py-5">
-            <h1 class="text-body-emphasis text-dark ">Trouvez un covoiturage</h1>
-            <p class="col-lg-8 mx-auto lead">
-                La solution accessible et durable pour tous.
-            </p>
+<section class="hero">
+    <div class="px-4 py-5 my-5 text-center text-dark">
+        <h2 class="fw-bold text-dark">Trouvez un covoiturage</h2>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">La solution accessible et durable pour tous.</p>
+            <div class="search-bar row">
+                <div class="search-field col-md-4 align-items-center">
+                    <i class="geo bi bi-geo-alt-fill text-primary"></i>
+                    <input type="text" name="depart" class="text-center" placeholder="Ville de départ" required="">
+                </div>
+                <div class="search-field col-md-4">
+                    <i class="geo bi bi-geo-alt text-primary"></i>
+                    <input type="text" name="arrivee" class="text-center" placeholder="Ville d'arrivée" required="">
+                </div>
+                <div class="d-flex col-md-4 justify-content-center">
+                    <button type="button" class="btn text-dark bg-primary justify-content-start ">Lancer la recherche<i class="bi bi-search text-white ms-2"></i></button>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+</section>
 <!--end section search -->
 
 
--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ECORIDE</title>
+    <title>EcoRide</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -31,4 +31,5 @@
             <button type="button" class="btn btn-outline-dark">Se connecter</button>
         </div>
     </header>
+
     <!-- end section Header -->
