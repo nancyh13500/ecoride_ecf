366698c8457ce860c9ebb589ace278561be23b45
nancyh13500 <nancy.hanoyan@free.fr>
2025-03-23 16:29:03 +0100

    ajout templates header and footer

---
+++ templates/footer.php
@@ -0,0 +1,27 @@
+<!-- section footer-->
+<div class="footer">
+    <footer class="d-flex flex-wrap justify-content-center align-items-center">
+        <a href="/index.php" class="col-md-12 d-flex align-items-center justify-content-center">
+            <img src="/assets/logo/logo_fond_dark.png" alt="logo checkit" width="120">
+        </a>
+        <p class="col-md-12 text-center text-white align-items-center text-body-dark mt-3">
+            Ecoride votre partenaire de covoiturage régulier domicile travail à Marseille, ses alentours et dans toute la France.Un site dédié au covoiturage en semaine et le week-end.
+        </p>
+        <p class="col-md-4 mb-0 text-body-secondary"></p>
+
+        <p class="col-md-4 text-center text-white align-items-center text-body-dark mt-3">by Nancy WebAgency @2025</p>
+
+        <ul class="nav col-md-4 justify-content-center">
+            <li class="ms-3"><a class="text-white" href="#"><i class="bi bi-facebook"></i></a></li>
+            <li class="ms-3"><a class="text-white" href="#"><i class="bi bi-instagram"></i></a></li>
+            <li class="ms-3"><a class="text-white" href="#"><i class="bi bi-envelope-open"></i></i></a></li>
+        </ul>
+    </footer>
+</div>
+<!-- end section footer-->
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
