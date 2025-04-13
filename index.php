ae6b353e8157353d949d445088fc278e78cd7afc
nancyh13500 <nancy.hanoyan@free.fr>
2025-04-13 22:16:21 +0200

    ajout section avis

--- index.php
+++ index.php
@@ -138,8 +138,42 @@
 <!-- end section proposition-->
 
 <!-- section avis -->
+<section class="avis">
+    <h1 class="text_avis text-center py-4">Chaque avis nous donne un peu plus envie</h1>
+    <div class="row row-cols-md-4 align-items-between py-5">
+        <div class="col">
+            <div class="card card-cover h-100 rounded-4 shadow-lg">
+                <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+                    <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h3>
+                </div>
+            </div>
+        </div>
 
+        <div class="col">
+            <div class="card card-cover h-100 rounded-4 shadow-lg">
+                <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+                    <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Much longer title that wraps to multiple lines</h3>
+                </div>
+            </div>
+        </div>
 
+        <div class="col">
+            <div class="card card-cover h-100 rounded-4 shadow-lg">
+                <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+                    <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Another longer title belongs here</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card card-cover h-100 rounded-4 shadow-lg">
+                <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+                    <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Another longer title belongs here</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
