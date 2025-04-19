ba1cf4d45f407189d1a405ef986de533fb466dc4
nancyh13500 <nancy.hanoyan@free.fr>
2025-04-19 23:15:28 +0200

    update login and add mediaquieries

--- login.php
+++ login.php
@@ -1,47 +1,50 @@
 <?php require_once __DIR__ . "/templates/header.php";
 ?>
 
-<main class="container py-5">
-    <div class="row justify-content-center">
-        <div class="col-md-4">
-            <div class="bg-dark text-white p-4 text-center mb-4">
-                <h2>S'identifier</h2>
-                <p class="mb-0">
-                    Pas encore membre,
-                    <a href="#" class="inscription text-secondary fw-bold text-decoration-underline">inscrivez-vous</a>
-                </p>
-            </div>
+<main class="login-hero">
+    <div class="login-background"></div>
+    <div class="content py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-3">
+                <div class="bg-dark text-white p-4 text-center mb-4">
+                    <h2>S'identifier</h2>
+                    <p class="mb-0">
+                        Pas encore membre,
+                        <a href="#" class="inscription text-secondary fw-bold text-decoration-underline">inscrivez-vous</a>
+                    </p>
+                </div>
 
-            <section class="pb-4">
-                <div class="border rounded-5">
-                    <section class="w-100 p-4 d-flex justify-content-center pb-4">
-                        <form class="login-form" method="POST" action="login.php">
-                            <div class="form-outline mb-4">
-                                <input type="email" class="form-control bg-light" placeholder="Email" required>
-                                <label class="form-label" for="form2Example1"></label>
-
+                <section class="pb-4">
+                    <div class="border border-dark rounded-5">
+                        <section class="w-100 p-4 d-flex justify-content-center pb-4">
+                            <form class="login-form" method="POST" action="login.php">
                                 <div class="form-outline mb-4">
-                                    <input type="password" class="form-control bg-light" placeholder="Mot de passe" required>
-                                    <label class="form-label" for="form2Example2"></label>
-                                </div>
+                                    <input type="email" class="form-control bg-light border-dark" placeholder="Email" required>
+                                    <label class="form-label" for="form2Example1"></label>
 
-                                <div class="row mb-4">
-                                    <div class="col d-flex justify-content-start">
-                                        <div class="form-check">
-                                            <input class="form-check-input border-dark" type="checkbox" value="">
-                                            <label class="form-check-label">Rester connecté</label>
+                                    <div class="form-outline mb-4">
+                                        <input type="password" class="form-control bg-light border-dark" placeholder="Mot de passe" required>
+                                        <label class="form-label" for="form2Example2"></label>
+                                    </div>
+
+                                    <div class="row mb-4">
+                                        <div class="col d-flex justify-content-start">
+                                            <div class="form-check">
+                                                <input class="form-check-input border-dark" type="checkbox" value="">
+                                                <label class="form-check-label">Rester connecté</label>
+                                            </div>
+                                        </div>
+                                        <div class="col d-flex justify-content-center">
+                                            <a class="" href="#!">Mot de passe oublié</a>
                                         </div>
                                     </div>
-                                    <div class="col d-flex justify-content-center">
-                                        <a class="" href="#!">Mot de passe oublié</a>
-                                    </div>
-                                </div>
 
-                                <button type="submit" class="btn btn-primary btn-block d-flex text-dark justify-content-center w-100 mb-4">Se connecter</button>
-                        </form>
-                    </section>
-                </div>
-            </section>
+                                    <button type="submit" class="btn btn-primary btn-block d-flex text-dark justify-content-center w-100 mb-4">Se connecter</button>
+                            </form>
+                        </section>
+                    </div>
+                </section>
+            </div>
         </div>
     </div>
 </main>
