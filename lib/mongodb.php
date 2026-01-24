3153527d0039b0953252c97919193e02f6a8b8f6
nancyh13500 <nancy.hanoyan@free.fr>
2026-01-24 17:35:06 +0100

    preparation deploiement avec portainer

--- lib/mongodb.php
+++ lib/mongodb.php
@@ -5,23 +5,35 @@ require_once __DIR__ . '/config.php';
 use MongoDB\Client;
 use MongoDB\Exception\Exception as MongoDBException;
 
+// Fonction helper pour récupérer les variables d'environnement MongoDB
+function getMongoEnvVar($key, $default = '')
+{
+    // Priorité : $_ENV > getenv() > $default
+    if (isset($_ENV[$key]) && !empty($_ENV[$key])) {
+        return $_ENV[$key];
+    }
+    $value = getenv($key);
+    return $value !== false ? $value : $default;
+}
+
 // Configuration MongoDB selon l'environnement
-$isDocker = getenv('DOCKER_ENV') || file_exists('/.dockerenv');
+$isDocker = getMongoEnvVar('DOCKER_ENV') || file_exists('/.dockerenv');
 
 if ($isDocker) {
     // Configuration Docker
-    define('MONGO_HOST', 'mongodb');
-    define('MONGO_PORT', '27017');
-    define('MONGO_DB', 'ecoride');
-    define('MONGO_USER', 'mongodb_user');
-    define('MONGO_PASS', 'mongodb_pass');
+    define('MONGO_HOST', getMongoEnvVar('MONGO_HOST', 'mongodb'));
+    define('MONGO_PORT', getMongoEnvVar('MONGO_PORT', '27017'));
+    define('MONGO_DB', getMongoEnvVar('MONGO_DB', 'ecoride'));
+    define('MONGO_USER', getMongoEnvVar('MONGO_USER', 'mongodb_user'));
+    define('MONGO_PASS', getMongoEnvVar('MONGO_PASS', 'mongodb_pass'));
 } else {
-    // Configuration locale
-    define('MONGO_HOST', 'localhost');
-    define('MONGO_PORT', '27017');
-    define('MONGO_DB', 'ecoride');
-    define('MONGO_USER', '');
-    define('MONGO_PASS', '');
+    // Configuration production ou locale (non-Docker)
+    // Les variables d'environnement sont prioritaires
+    define('MONGO_HOST', getMongoEnvVar('MONGO_HOST', 'localhost'));
+    define('MONGO_PORT', getMongoEnvVar('MONGO_PORT', '27017'));
+    define('MONGO_DB', getMongoEnvVar('MONGO_DB', 'ecoride'));
+    define('MONGO_USER', getMongoEnvVar('MONGO_USER', ''));
+    define('MONGO_PASS', getMongoEnvVar('MONGO_PASS', ''));
 }
 
 // Connexion MongoDB
